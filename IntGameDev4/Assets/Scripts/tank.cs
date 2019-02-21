20b6440c30fd5f315459042d4980f5f2fd862971
Winlyn Liu <wl1698@nyu.edu>
2019-02-21 11:28:25 -0500

    first commit

---
+++ IntGameDev4/Assets/Scripts/tank.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tank : MonoBehaviour
+{
+	public Rigidbody thisRigidBody;
+	public Vector2 inputAxis;
+	public int forceMultiplyer;
+	public float tankVelocity;
+	public float inputToTorque;
+	public float torqueMultiplyer;
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		inputAxis.y = Input.GetAxis("Vertical");
+		inputAxis.x = Input.GetAxis("Horizontal");
+
+		inputToTorque = inputAxis.x;
+
+	}
+
+	void FixedUpdate()
+	{
+		tankVelocity = thisRigidBody.velocity.magnitude;
+		if (thisRigidBody.velocity.magnitude < 8f)
+		{
+			thisRigidBody.AddForce(transform.forward * inputAxis.y * forceMultiplyer, ForceMode.Impulse);
+		}
+		thisRigidBody.AddTorque(0, inputToTorque * torqueMultiplyer, 0, ForceMode.VelocityChange);
+	}
+}
